278d028fccea1ac40f075c8f663a58a1129e89b7
jiang <344898012@qq.com>
2019-08-09 20:20:19 +0800

    svg修复

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,24 +1,8 @@
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-import store, { types } from '../../store'
-const requireAll = requireContext => requireContext.keys().map(requireContext)
-const req = require.context('./svg', true, /\.svg$/)
-requireAll(req)
-=======
-=======
->>>>>>> bf03f9217fe57dfc390a771fc16e791e541c6110
+
 /**
  * 依赖svg-sprite-loader
  * 介绍：https: //segmentfault.com/a/1190000015367490#articleHeader3
  */
-<<<<<<< HEAD
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
-import store, { types } from '../../store';
->>>>>>> bf03f9217fe57dfc390a771fc16e791e541c6110
 
 // 获取所有svg文件内容
 const requireAll = requireContext => requireContext.keys().map(requireContext);
